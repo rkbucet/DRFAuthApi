47352bc6076e8e667fda813eca4fff178b66623c
Rahul Kumar Bhagat <rkb.ucet@gmail.com>
2024-01-12 00:17:58 +0530

    Custom User Model created

---
+++ authapi/account/urls.py
@@ -0,0 +1,6 @@
+# from django.urls import path
+# from account import views
+
+# urlpatterns = [
+#     path('register', views.rergister, name='register'),
+# ]
--- authapi/authapi/settings.py
+++ authapi/authapi/settings.py
@@ -138,3 +138,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
+
+AUTH_USER_MODEL = 'account.User'
