492f4524dbc9ed61be582bb24e5d7d8af32217a7
Rahul Kumar Bhagat <rkb.ucet@gmail.com>
2024-01-13 01:10:43 +0530

    Revert "Revert "user Login view, serializer and url added""
    This reverts commit 8e046528e04ca534523b4dbe6199c2ae5f745aca.

--- authapi/account/views.py
+++ authapi/account/views.py
@@ -1,9 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from account.serializers import UserRegistrationSerializer
+from account.serializers import UserRegistrationSerializer, UserLoginSerializer
 from rest_framework import status
 from account.renderers import UserRenderer
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 
 def get_tokens_for_user(user):
   refresh = RefreshToken.for_user(user)
@@ -21,3 +22,17 @@ class UserRegistrationView(APIView):
     user = serializer.save()
     token = get_tokens_for_user(user)
     return Response({'token':token, 'msg':'Registration Successful'}, status=status.HTTP_201_CREATED)
+
+
+class UserLoginView(APIView):
+  renderer_classes = [UserRenderer]
+  def post(self, request, format=None):
+    serializer = UserLoginSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    email = serializer.data.get('email')
+    password = serializer.data.get('password')
+    user = authenticate(email=email, password=password)
+    if user is not None:
+      token = get_tokens_for_user(user)
+      return Response({"token":token, 'msg':'Login Successful'}, status=status.HTTP_200_OK)
+    return Response({{'errors:''non_field_errors':['Email or Password is not valid']}},status=status.HTTP_404_NOT_FOUND)
