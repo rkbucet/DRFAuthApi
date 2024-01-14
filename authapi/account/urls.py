2ac21495bafad2eb49a5c4de8f65dab3052fc570
Rahul Kumar Bhagat <rkb.ucet@gmail.com>
2024-01-15 00:15:04 +0530

    changePassword view added

--- authapi/account/urls.py
+++ authapi/account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from account.views import UserRegistrationView, UserLoginView, UserProfileView
+from account.views import UserRegistrationView, UserLoginView, UserProfileView, UserChangePasswordView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('change-password/', UserChangePasswordView.as_view(), name='change-password'),
+
 ]
--- authapi/account/views.py
+++ authapi/account/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from account.serializers import UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer
+from account.serializers import UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer, UserChangePasswordSerializer
 from rest_framework import status
 from account.renderers import UserRenderer
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -45,3 +45,12 @@ class UserProfileView(APIView):
   def get(self, request, format=None):
     serializer = UserProfileSerializer(request.user)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserChangePasswordView(APIView):
+  renderer_classes = [UserRenderer]
+  permission_classes = [IsAuthenticated]
+  def post(self, request, format=None):
+    serializer = UserChangePasswordSerializer(data=request.data, context={'user':request.user})
+    serializer.is_valid(raise_exception=True)
+    return Response({'msg':'Password Changed Successfully'}, status=status.HTTP_200_OK)
